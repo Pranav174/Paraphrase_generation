c90901c6abc1d8c72aad8bcc5962b1e1a3f4c1bb
Pranav174 <pranav.goyal@research.iiit.ac.in>
2019-11-19 09:26:16 +0530

    runner

--- runner.py
+++ runner.py
@@ -22,9 +22,10 @@ for f in os.listdir("DATA/PROCESSED-DATA/collection/"):
             originals.append(i.word)
 
     for sentence in var.sentenceList:
-        # start = sentence.wordNumList[0]
-        # end = sentence.wordNumList[-1]+1
         # answer = replace_antonyms(sentence, var.globalWordList, originals, False)
+        # answer = replace_antonyms(sentence, var.globalWordList, originals, True)
+        # answer = replace_synonyms(sentence, var.globalWordList, originals, False)
+        # answer = replace_synonyms(sentence, var.globalWordList, originals, True)
         # answer = re_arrange(sentence, var.globalWordList)
         answer = kritvachya_to_karmvachya(sentence, var.globalWordList, originals)
         if len(answer):
@@ -39,35 +40,5 @@ for f in os.listdir("DATA/PROCESSED-DATA/collection/"):
             print()
         if len(answer):
             print()
-
-
-
-    # for sentence in var.sentenceList:
-    #     count = 0
-    #     for tag in sentence.nodeDict.keys():
-    #         if sentence.nodeDict[tag].nodeParent == 'None' and sentence.nodeDict[tag].chunkNum != -1:
-    #             count+=1
-    #     if count>1:
-    #         print(count)
-    #         print("SENTENCE:")
-    #         for word in sentence.wordNumList:
-    #             print(var.globalWordList[word].word, end=" ")
-    #         print()
-    #         for word in sentence.wordNumList:
-    #             print(var.globalWordList[word].featureSet.__dict__)
-    #         print()
-    #         print(sentence.__dict__)
-    #         print("nodes:")
-    #         for tag in sentence.nodeDict.keys():
-    #             print(tag, '  ' , sentence.nodeDict[tag].__dict__)
-    #             # print(tag)
-    #         print("chunks")
-    #         for chunk in sentence.chunkList:
-    #             print(chunk.__dict__)
-    #         for root in sentence.rootNode:
-    #             print(root)
-    #         print()
-
-    # print("\n")
 
 
